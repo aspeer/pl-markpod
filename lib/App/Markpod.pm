fe507908d53950186d7eeb6c97cfe71ab1dfaba3
Andrew Speer <andrew.speer@isolutions.com.au>
2024-11-17 18:49:44 +1030

    Interim commit

--- lib/App/Markpod.pm
+++ lib/App/Markpod.pm
@@ -254,7 +254,6 @@ foreach my $sub (qw(markdown pod ppi_doc_or)) {
 1;
 __END__
 
-=pod
 =begin markdown
 
 # NAME
@@ -279,13 +278,17 @@ Helper module for the markpod utility that can also be used independently.
 
 # METHODS
 
-**new()** Create a new App::Markpod reference. Usage:
+**new()** 
+
+Create a new App::Markpod reference. Usage:
 
 `my $markpod_or=App::Markpod->new(\%opt)`
 
 See OPTIONS section for options that can be supplied to creator
 
-**markpod_process( filename )** Process the named file. Will return number of POD lines changed from any existing in file as scalar reference
+**markpod_process( filename )** 
+
+Process the named file. Will return number of POD lines changed from any existing in file as scalar reference
 
 ```perl
 my $lines_changed_sr=$markpod_or->markpod_process('foo.pl');
@@ -310,3 +313,62 @@ Full license text is available at:
 <http://dev.perl.org/licenses/>
 
 =end markdown
+
+
+=head1 NAME
+
+App::Markpod - convert markdown formatted pod to pure pod
+
+
+=head1 SYNOPSIS
+
+
+ 
+ use App::Markpod
+ my $markpod_or=App::Markpod ->new()
+ $markpod_or->markpod_process('foo.pl');
+ my $pod_sr=$markpod_or->pod()
+ print ${$pod_sr}
+
+=head1 DESCRIPTION
+
+Helper module for the markpod utility that can also be used independently. 
+
+
+=head1 METHODS
+
+B<new()> 
+
+Create a new App::Markpod reference. Usage:
+
+C<<< my $markpod_or=App::Markpod->new(\%opt) >>>
+
+See OPTIONS section for options that can be supplied to creator
+
+B<markpod_process( filename )> 
+
+Process the named file. Will return number of POD lines changed from any existing in file as scalar reference
+
+
+ my $lines_changed_sr=$markpod_or->markpod_process('foo.pl');
+ print "lines changed: ", ${$lines_changed_sr}, "\n";
+
+=head1 AUTHOR
+
+Andrew Speer L<mailto:andrew.speer@isolutions.com.au>
+
+
+=head1 LICENSE and COPYRIGHT
+
+This file is part of markpod.
+
+This software is copyright (c) 2024 by Andrew Speer L<mailto:andrew.speer@isolutions.com.au>.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+Full license text is available at:
+
+L<http://dev.perl.org/licenses/>
+
+=cut
